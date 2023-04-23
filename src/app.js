034432aeacf8e541298ac11b380198ec67a4ca06
Manash2712 <manashchauhan2712@gmail.com>
2023-04-23 18:25:12 +0530

    added contact us page and used Outlet for conditional routing

--- src/app.js
+++ src/app.js
@@ -32,16 +32,18 @@ import { Title } from "./components/Header"
 import Body from "./components/Body"
 import Footer from "./components/Footer";
 import { IMG_CDN_URL } from "./config";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error"
+import Contact from "./components/Contact";
 
 
 const AppLayout = () => {
     return (
         <>
             <HeaderComponent />
-            <Body />
+            {/* {Outlet} */}
+            <Outlet />
             <Footer />
         </>
     )
@@ -51,12 +53,26 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
-        errorElement: <Error />
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ]
     },
-    {
-        path: "/about",
-        element: <About />
-    }
+    // {
+    //     path: "/about",
+    //     element: <About />
+    // }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ const HeaderComponent = () => {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <li>Contact</li>
+                    <li><Link to="/contact">Contact</Link></li>
                     <li>Cart</li>
                 </ul>
             </div>
