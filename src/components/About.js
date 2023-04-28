ff7cd6497b30895438393b0833058428f922d8de
Manash2712 <manashchauhan2712@gmail.com>
2023-04-28 16:05:39 +0530

    learnt lifecycle methods

--- src/components/About.js
+++ src/components/About.js
@@ -35,7 +35,7 @@ class About extends React.Component {
                 <h1>About Us Page</h1>
                 <p>This is the Namaste React Live course chapter seven</p>
                 <ProfileClass name={"First Child"} />
-                <ProfileClass name={"Second Child"} />
+                {/* <ProfileClass name={"Second Child"} /> */}
             </div>
         )
     }
@@ -47,6 +47,31 @@ export default About
 
 /*
 
+Parent-Constructor
+Parent-render
+Child-constructor First Child
+Child-render First Child
+Child-constructor Second Child
+Child-render Second Child
+Child-ComponentDidMount First Child
+Child-ComponentDidMount Second Child
+Parent-componentDidMount
 
+*/
+
+/*
+After calling api inside componentDidMount of Profile
+
+Parent-Constructor
+Parent-render
+Child-constructor First Child
+Child-render First Child
+Parent-componentDidMount
+Child-ComponentDidMount First Child
+Child-render First Child
+
+
+react finishes render phase first and then goes to commit phase. 
+Child-ComponentDidMount will be called later because it is async and it will take some time and data to load
 
 */
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -3,30 +3,71 @@ import React from "react";
 class Profile extends React.Component {
     constructor(props) {
         super(props);
+        // this.state = {
+        //     count: 0,
+        //     count2: 0,
+        // }
         this.state = {
-            count: 0,
-            count2: 0,
+            userInfo: {
+                name: "Dummy Name",
+                location: "Dummy Location",
+            }
         }
         console.log("Child-constructor " + this.props.name);
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        // Api Call
+        const data = await fetch("https://api.github.com/users/akshaymarch7")
+        const json = await data.json();
+        this.setState({
+            userInfo: json,
+        })
         console.log("Child-ComponentDidMount " + this.props.name);
+    }
+
+    componentDidUpdate() {
+        console.log("Child ComponentDidUpdate");
     }
     render() {
         console.log("Child-render " + this.props.name);
+        // return (
+        //     <div>
+        //         <h1>Profile Class Component</h1>
+        //         <h3>Name: {this.props.name}</h3>
+        //         <h2>Count: {this.state.count}</h2>
+        //         <button onClick={() => this.setState({
+        //             count: 1,
+        //             count2: 2,
+        //         })}>SetCount</button>
+        //     </div>
+        // )
         return (
             <div>
                 <h1>Profile Class Component</h1>
-                <h3>Name: {this.props.name}</h3>
-                <h2>Count: {this.state.count}</h2>
-                <button onClick={() => this.setState({
-                    count: 1,
-                    count2: 2,
-                })}>SetCount</button>
+                <img src={this.state.userInfo.avatar_url} alt="" />
+                <h2>Name: {this.state.userInfo.name}</h2>
+                <h2>Location: {this.state.userInfo.location}</h2>
             </div>
         )
     }
 }
 
 export default Profile;
+
+
+/*
+
+constructor
+render
+componentDidMount  - after first render
+
+API call
+set state
+
+<Update Cycles>
+render
+
+componentDidUpdate  - after every next render
+
+*/
