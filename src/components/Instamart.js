6287f82731f1882135e65c17212d41b6d0628b9e
Manash2712 <manashchauhan2712@gmail.com>
2023-05-02 00:20:36 +0530

    added diffrent sextions inside instamart component

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-const Section = ({ title, description }) => {
-    const [isVisible, setIsVisible] = useState(false);
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+
     return (
         <div className="border border-black p-2 m-2">
             <h3 className="font-bold text-xl">{title}</h3>
@@ -16,6 +16,14 @@ const Section = ({ title, description }) => {
 }
 
 const Instamart = () => {
+    const [sectionConfig, setSectionConfig] = useState({
+        showAbout: true,
+        showTeam: false,
+        showCareers: true,
+        showProduct: true,
+        showDetails: true,
+    })
+    // const [isVisible, setIsVisible] = useState(false);
     return (
         <div>
             <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
@@ -24,6 +32,16 @@ const Instamart = () => {
                 description={
                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos temporibus nulla quis voluptates minima minus laudantium exercitationem officiis culpa soluta eveniet nihil commodi molestiae, deserunt ducimus beatae praesentium iusto cupiditate. Vitae voluptate voluptatum, temporibus laboriosam nisi, repudiandae inventore hic incidunt quam corrupti sint. Recusandae a veniam necessitatibus optio culpa omnis cupiditate. Pariatur facere soluta voluptas quasi porro atque temporibus!"
                 }
+                isVisible={sectionConfig.showAbout}
+                setIsVisible={() => {
+                    setSectionConfig({
+                        showAbout: true,
+                        showTeam: false,
+                        showCareers: false,
+                        showProduct: false,
+                        showDetails: false,
+                    })
+                }}
             />
 
             <Section
@@ -31,11 +49,66 @@ const Instamart = () => {
                 description={
                     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos temporibus nulla quis voluptates minima minus laudantium exercitationem officiis culpa soluta eveniet nihil commodi molestiae, deserunt ducimus beatae praesentium iusto cupiditate. Vitae voluptate voluptatum, temporibus laboriosam nisi, repudiandae inventore hic incidunt quam corrupti sint. Recusandae a veniam necessitatibus optio culpa omnis cupiditate. Pariatur facere soluta voluptas quasi porro atque temporibus!"
                 }
+                isVisible={sectionConfig.showTeam}
+                setIsVisible={() => {
+                    setSectionConfig({
+                        showAbout: false,
+                        showTeam: true,
+                        showCareers: false,
+                        showProduct: false,
+                        showDetails: false,
+                    })
+                }}
             />
 
             <Section
                 title={"Careers"}
+                description={
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos temporibus nulla quis voluptates minima minus laudantium exercitationem officiis culpa soluta eveniet nihil commodi molestiae, deserunt ducimus beatae praesentium iusto cupiditate. Vitae voluptate voluptatum, temporibus laboriosam nisi, repudiandae inventore hic incidunt quam corrupti sint. Recusandae a veniam necessitatibus optio culpa omnis cupiditate. Pariatur facere soluta voluptas quasi porro atque temporibus!"
+                }
+                isVisible={sectionConfig.showCareers}
+                setIsVisible={() => {
+                    setSectionConfig({
+                        showAbout: false,
+                        showTeam: false,
+                        showCareers: true,
+                        showProduct: false,
+                        showDetails: false,
+                    })
+                }}
+            />
+            <Section
+                title={"Product"}
+                description={
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos temporibus nulla quis voluptates minima minus laudantium exercitationem officiis culpa soluta eveniet nihil commodi molestiae, deserunt ducimus beatae praesentium iusto cupiditate. Vitae voluptate voluptatum, temporibus laboriosam nisi, repudiandae inventore hic incidunt quam corrupti sint. Recusandae a veniam necessitatibus optio culpa omnis cupiditate. Pariatur facere soluta voluptas quasi porro atque temporibus!"
+                }
+                isVisible={sectionConfig.showProduct}
+                setIsVisible={() => {
+                    setSectionConfig({
+                        showAbout: false,
+                        showTeam: false,
+                        showCareers: false,
+                        showProduct: true,
+                        showDetails: false,
+                    })
+                }}
+            />
 
+            <Section
+                title={"Details "}
+                description={
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos temporibus nulla quis voluptates minima minus laudantium exercitationem officiis culpa soluta eveniet nihil commodi molestiae, deserunt ducimus beatae praesentium iusto cupiditate. Vitae voluptate voluptatum, temporibus laboriosam nisi, repudiandae inventore hic incidunt quam corrupti sint. Recusandae a veniam necessitatibus optio culpa omnis cupiditate. Pariatur facere soluta voluptas quasi porro atque temporibus!"
+                }
+                isVisible={sectionConfig.showDetails}
+                setIsVisible={() => {
+                    setSectionConfig({
+                        showAbout: false,
+                        showTeam: false,
+                        showCareers: false,
+                        showProduct: false,
+                        showDetails: true,
+                    })
+                }}
             />
         </div>
     )
