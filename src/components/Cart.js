6fe0a956dfdf76cd9ec1e88528e029ee1f06627c
Manash2712 <manashchauhan2712@gmail.com>
2023-05-05 12:08:51 +0530

    added clear cart button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,22 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import FoodItem from "./FoodItem"
+import { clearCart } from "../utils/cartSlice"
 
 const Cart = () => {
     const cartItems = useSelector(store => store.cart.items) // subscribing to the specific portion of store
-    console.log('cartIems isde cart', cartItems);
+    // console.log('cartIems isde cart', cartItems);
+
+    const dispatch = useDispatch();
+    const handleClearCart = () => {
+        dispatch(clearCart())
+    }
     return (
         <div>
             <h1 className="font-bold text-3xl">Cart Items - {cartItems.length}</h1>
-            {cartItems.map((cartItem) => <FoodItem key={cartItem.id} {...cartItem} />)}
+            <button className="bg-green-100 p-2 m-5" onClick={() => handleClearCart()}>Clear Cart</button>
+            <div className="flex">
+                {cartItems.map((cartItem) => <FoodItem key={cartItem.id} {...cartItem} />)}
+            </div>
 
         </div>
     )
